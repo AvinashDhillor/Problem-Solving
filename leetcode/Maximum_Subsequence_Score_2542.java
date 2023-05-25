4fa15ba7cf60ebac12e24ac15e5edea5441e70bd
Avinash Dhillor <avinashdhillor24@gmail.com>
2023-05-25 23:10:37 +0530

    commited on -25-05-2023- by Avinash Dhillor

--- leetcode/Maximum_Subsequence_Score_2542.java
+++ leetcode/Maximum_Subsequence_Score_2542.java
@@ -1,11 +1,6 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.TreeMap;
-
-import javax.swing.text.html.HTMLDocument.HTMLReader.PreAction;
 
 public class Maximum_Subsequence_Score_2542 {
 
@@ -19,8 +14,8 @@ public class Maximum_Subsequence_Score_2542 {
         }
 
         @Override
-        public int compareTo(Maximum_Subsequence_Score_2542.Pair o) {
-            return o.x - x;
+        public int compareTo(Pair o) {
+            return o.y - y;
         }
 
         @Override
@@ -31,29 +26,37 @@ public class Maximum_Subsequence_Score_2542 {
     }
 
     public static long maxScore(int[] nums1, int[] nums2, int k) {
-        List<Pair> list = new ArrayList<>();
-        for (int i = 0; i < nums1.length; i++) {
-            list.add(new Pair(nums2[i], nums1[i]));
+        int n = nums1.length;
+
+        Pair pairs[] = new Pair[n];
+
+        for (int i = 0; i < n; i++) {
+            pairs[i] = new Pair(nums1[i], nums2[i]);
         }
 
-        Collections.sort(list);
+        Arrays.sort(pairs);
 
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-        long sum = 0;
+        System.out.println(Arrays.toString(pairs));
+
+        PriorityQueue<Integer> pQueue = new PriorityQueue<>(k);
+
+        long topSum = 0;
 
         for (int i = 0; i < k; i++) {
-            Pair pair = list.get(i);
-            priorityQueue.add(pair.y);
-
-            if (priorityQueue.size() > k)
-                priorityQueue.remove();
+            Pair p = pairs[i];
+            topSum = p.x;
+            pQueue.add(p.x);
         }
 
-        for (int i : priorityQueue) {
-            sum += i;
+        long ans = topSum * pairs[k - 1].y;
+
+        for (int i = k; i < n; i++) {
+            topSum += pairs[i].x - pQueue.poll();
+            pQueue.add(pairs[i].x);
+            ans = Math.max(ans, topSum * pairs[i].y);
         }
 
-        return 0L;
+        return ans;
     }
 
     public static void main(String[] args) {
