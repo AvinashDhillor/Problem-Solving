0044c62d8e0b5c81f181d7b7589621ff7c20492c
Avinash Dhillor <avinashdhillor24@gmail.com>
2023-06-05 07:24:10 +0530

    commited on -05-06-2023- by Avinash Dhillor

--- leetcode/Time_Needed_to_Inform_All_Employees_1376.java
+++ leetcode/Time_Needed_to_Inform_All_Employees_1376.java
@@ -6,27 +6,30 @@ import java.util.List;
  * Time_Needed_to_Inform_All_Employees_1376
  */
 public class Time_Needed_to_Inform_All_Employees_1376 {
-    static int sum = 0;
+    int maxTime = Integer.MIN_VALUE;
 
-    public static int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
         List<Integer>[] arr = new ArrayList[n];
 
+        for (int i = 0; i < n; i++)
+            arr[i] = new ArrayList<Integer>();
+
         for (int i = 0; i < manager.length; i++) {
+            if (manager[i] == -1)
+                continue;
             arr[manager[i]].add(i);
         }
 
-        cal(arr, headID, informTime);
+        cal(arr, headID, informTime, 0);
 
-        System.out.println(Arrays.toString(arr));
-
-        return sum;
+        return maxTime;
     }
 
-    private static void cal(List<Integer>[] arr, int headID, int[] informTime) {
-        sum += informTime[headID];
+    private void cal(List<Integer>[] arr, int headID, int[] informTime, int time) {
+        maxTime = Math.max(maxTime, time);
+
         for (int i : arr[headID]) {
-            cal(arr, i, informTime);
+            cal(arr, i, informTime, time + informTime[headID]);
         }
     }
-
 }
