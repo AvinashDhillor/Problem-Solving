f8b6af6d21a76ee5ad026a0362dc748d8b84f7a7
Avinash Dhillor <avinashdhillor24@gmail.com>
2023-06-12 19:19:10 +0530

    Commited on 2023-06-12 by Avinash Dhillor

--- leetcode/Snapshot_Array_1146.java
+++ leetcode/Snapshot_Array_1146.java
@@ -1,19 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
 class SnapshotArray {
 
-    public SnapshotArray(int length) {
+    TreeMap<Integer, Integer>[] map;
+    int snap_id = 0;
 
+    public SnapshotArray(int length) {
+        map = new TreeMap[length];
+        for (int i = 0; i < length; i++) {
+            map[i] = new TreeMap<>();
+            map[i].put(0, 0);
+        }
     }
 
     public void set(int index, int val) {
-
+        map[index].put(snap_id, val);
     }
 
     public int snap() {
-        return 0;
+        return snap_id++;
     }
 
     public int get(int index, int snap_id) {
-        return 0;
+        return map[index].floorEntry(snap_id).getValue();
     }
 }
 
